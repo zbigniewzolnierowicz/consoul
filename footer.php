aaafc5dc616b6bc20c6324e54066d190fcaa55f8
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2020-12-29 18:16:43 +0100

    Fix cursor in footer

--- footer.php
+++ footer.php
@@ -9,8 +9,8 @@
 
 </main>
 
-<footer class="footer cursor">
-    <p class="footer__text"><span class="footer__account">zbigniew</span><span class="footer__at">@</span><span class="footer__hostname">zolnierowicz</span>:<span class="footer__directory">/srv/www/wordpress</span>$</p>
+<footer class="footer">
+    <p class="footer__text cursor"><span class="footer__account">zbigniew</span><span class="footer__at">@</span><span class="footer__hostname">zolnierowicz</span>:<span class="footer__directory">/srv/www/wordpress</span>$</p>
 </footer>
     
 <?php wp_footer(); ?>
